fe98922d765c20d4dcd242780793cf812157d3c5
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-12 11:40:40 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+# WAP to obtain 3 number and print their number
+
+num1=int(input("Enter a 1st Number : "))
+num2=int(input("enter a 2nd Number : "))
+num3=int(input("Enter a 3rd Number : "))
+
+sum=(num1+num2+num3)
+print("The 3 number is : ",num1,num2,num3)
+print("Sum is :",sum)
