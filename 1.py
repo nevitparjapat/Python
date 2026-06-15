d20cd8e519c17268b2ebe9237b526b35149870b2
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-15 09:54:51 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+# WAP traffic Signal Indicator
+
+Light=input("Light Colour : ")
+
+if(Light=="red"):
+    print("stop")
+elif(Light=="yellow"):
+    print("Look")
+elif(Light=="green"):
+    print("Goooo")
+else:
+    print("Light is broken")
+
+print("Drive Safelly")
