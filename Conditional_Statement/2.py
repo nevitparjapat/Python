ab3e724eb2291efbc6123ffaaa70cd28f111bb26
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-17 10:16:13 +0530

    Add files via upload

---
+++ Conditional_Statement/2.py
@@ -0,0 +1,19 @@
+# WAP to Create a Grade System 
+
+Marks=int(input("Enter a Student Marks : "))
+
+if(Marks>=90):
+    print(" A Grade")
+elif(Marks>=75):
+    print("B Grade")
+elif(Marks>=55):
+    print("C Grade")
+elif(Marks>=45):
+    print("D Grade")
+elif(Marks>=33):
+    print("E Grade")
+
+else:
+    print("YOU ARE FAIL")
+ 
+ 
