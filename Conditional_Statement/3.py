f519d96fd9949611661b8061eefb01fafad4219a
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-18 14:11:57 +0530

    Add files via upload

---
+++ Conditional_Statement/3.py
@@ -0,0 +1,10 @@
+#WAP to input 2 number which is greater
+
+a=int(input("Enter 1st Number : "))
+b=int(input("Enter 2nd Number : "))
+
+if(a>b):
+    print("a is greater")
+else:
+    print("b is greater")
+
