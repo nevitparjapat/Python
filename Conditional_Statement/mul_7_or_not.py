a910231646f4c0c22e506b8a5166a4e8f05ac741
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-21 01:34:13 +0530

    Add files via upload

---
+++ Conditional_Statement/mul_7_or_not.py
@@ -0,0 +1,9 @@
+# WAP to check if number is a multipal of 7 or not 
+
+a=int(input("Enter a Number : "))
+
+x=a%7
+if(x==0):
+    print("It is Multipal by 7")
+else:
+    print("It is not Multipal by 7")
