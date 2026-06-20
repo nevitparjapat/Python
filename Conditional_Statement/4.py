8d5b82657b51cc8b316ef1ba1de718c5502cfe9e
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-20 08:53:22 +0530

    Add files via upload

---
+++ Conditional_Statement/4.py
@@ -0,0 +1,11 @@
+#WAP to input 2 number a and b print True if a is greater then or equal to b 
+# if not print false
+
+a=int(input("Enter a 1st Number : "))
+b=int(input("Enter a 2nd Number : "))
+
+if(a>=b):
+    print("True")
+else:
+    print("False")
+
