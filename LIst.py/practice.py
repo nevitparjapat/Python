33461558498c961a0de84bc0517ce953054ee28e
Nevit Prajapati <nevitprajapati123@gmail.com>
2026-06-28 01:07:01 +0530

    Add files via upload

---
+++ LIst.py/practice.py
@@ -0,0 +1,12 @@
+# WAP to ask the user to enter name of their favourite movies and store them in list
+
+movie=[]
+mov1=input("Enter a Fvourite MOVIE : ")
+mov2=input("Enter a Fvourite MOVIE : ")
+mov3=input("Enter a Fvourite MOVIE : ")
+
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+
+print(movie)
